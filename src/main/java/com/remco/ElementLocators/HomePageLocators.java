2e224846d0c8ec09f622d340bf8f45a30cd44556
Venkateswarlu Tumpuru <Venkateswarlu.Tumpuru@HighJumpOnDemand.Com>
2024-01-23 02:10:49 -0500

    Removed static keyword from ElementLocators page

--- src/main/java/com/remco/ElementLocators/HomePageLocators.java
+++ src/main/java/com/remco/ElementLocators/HomePageLocators.java
@@ -2,12 +2,12 @@ package com.remco.ElementLocators;
 
 public interface HomePageLocators {
 	
-	static String hp_userName			=	"//input[@placeholder='User Name']";
-	static String hp_password			=	"//input[@placeholder='Password']";
-	static String hp_loginButton		=	"//button[@class='k-button']";
-	static String hp_accountIcon		=	"//i[@class='fa fa-user']";
-	static String hp_logoutText			=	"//span[text()='Logout']";
-	static String hp_confirmLogout_btn	=	"//button[@class='k-button']";
+	 String hp_userName				=	"//input[@placeholder='User Name']";
+	 String hp_password				=	"//input[@placeholder='Password']";
+	 String hp_loginButton			=	"//button[@class='k-button']";
+	 String hp_accountIcon			=	"//i[@class='fa fa-user']";
+	 String hp_logoutText			=	"//span[text()='Logout']";
+	 String hp_confirmLogout_btn	=	"//button[@class='k-button']";
 	
 	
 
