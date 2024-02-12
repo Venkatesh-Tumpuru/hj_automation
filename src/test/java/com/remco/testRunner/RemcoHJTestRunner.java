aae90cc1c752bcbd5e1e1cf6d7039cfff2985b55
Venkatesh Tumpuru <157343554+Venkatesh-Tumpuru@users.noreply.github.com>
2024-02-12 03:30:41 -0500

    Updated RemcoHJTestRunner.java

--- src/test/java/com/remco/testRunner/RemcoHJTestRunner.java
+++ src/test/java/com/remco/testRunner/RemcoHJTestRunner.java
@@ -5,6 +5,9 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.testng.annotations.*;
 
 import com.google.gson.JsonArray;
@@ -27,8 +30,16 @@ import io.cucumber.testng.CucumberOptions;
 public class RemcoHJTestRunner extends AbstractTestNGCucumberTests {
     static String JSON_FILE_PATH = "target/cucumber-reports/cucumber.json";
     static String HTML_FILE_PATH = "target/cucumber-reports/cucumber.html";
+    
+    Date date = new Date(); 
+    SimpleDateFormat dateFormat = new SimpleDateFormat("MMM dd, yyyy");
+    SimpleDateFormat timeFormat = new SimpleDateFormat("hh:mm a");
+    String formattedDate = dateFormat.format(date);
+    String formattedTime = timeFormat.format(date);
+    String actulaTime	= formattedDate+" ("+formattedTime+")";
+
 @AfterSuite(alwaysRun=true)
-public void generateHTMLReport() {
+public  void generateHTMLReport() {
     try {
         String filePath = JSON_FILE_PATH;
         // Read JSON data from file
@@ -49,7 +60,7 @@ public void generateHTMLReport() {
         JsonArray features = jsonElement.getAsJsonArray();
         StringBuilder htmlBuilder = new StringBuilder();
 
-        int totalFeatures = features.size();
+        int totalFeatures = 0;
         int totalPassedFeatures = 0;
         int totalFailedFeatures = 0;
         int totalScenarios = 0;
@@ -59,40 +70,26 @@ public void generateHTMLReport() {
         int totalPassedSteps = 0;
         int totalFailedSteps = 0;
 
+        htmlBuilder.append("<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"><title>Cucumber Report</title>");
+        htmlBuilder.append("<style>table {border-collapse: collapse;width: 80%;max-width: 600px;margin: 0 auto;} th, td {border: 1px solid #dddddd;text-align: left;padding: 8px;} th {background-color: #f2f2f2;}</style>");
+        htmlBuilder.append("</head><body>");
+
+        // Header
+        htmlBuilder.append("<header style='border-collapse: collapse;width: 80%;max-width: 600px;margin: 0 auto;'><p>Hi Team,</br>Greetings for the day.</br></br>Please find the below Automation status report</p></header>");
+        htmlBuilder.append("<h2 style='text-align:center;margin-bottom:20px;'>Automation Test Report - "+actulaTime+"</h2>");
+
         for (JsonElement featureElement : features) {
+            totalFeatures++; // Increment total features count
             JsonObject feature = featureElement.getAsJsonObject();
             String featureName = feature.get("name").getAsString();
             JsonArray scenarios = feature.getAsJsonArray("elements");
             boolean hasFailedScenario = false;
 
-            htmlBuilder.append("<table border='1' style='border-collapse: collapse;'>\n");
-            htmlBuilder.append("<thead><tr>");
-            boolean hasFailedStepInFeature = false; // Flag to check if any step has failed in the feature
-            for (JsonElement scenarioElement : scenarios) {
-                JsonObject scenario = scenarioElement.getAsJsonObject();
-                JsonArray steps = scenario.getAsJsonArray("steps");
-                for (JsonElement stepElement : steps) {
-                    JsonObject step = stepElement.getAsJsonObject();
-                    JsonObject result = step.getAsJsonObject("result");
-                    totalSteps++;
-                    String status = result.get("status").getAsString();
-                    if (status.equalsIgnoreCase("failed")) {
-                        hasFailedStepInFeature = true;
-                        totalFailedSteps++;
-                        break; // No need to check further steps if one has already failed
-                    }
-                }
-                if (hasFailedStepInFeature) {
-                    break; // No need to check further scenarios if one has already failed
-                }
-            }
-            // Set background color of feature header based on whether any scenario has failed
-            String featureHeaderColor = hasFailedStepInFeature ? "#F9502B" : "#2BF96D";
-            htmlBuilder.append("<th colspan='2' style='background-color: ").append(featureHeaderColor).append(";'>").append(featureName).append("</th>");
-            htmlBuilder.append("</tr></thead>\n");
-            htmlBuilder.append("<tbody>\n");
+            htmlBuilder.append("<table>");
+            htmlBuilder.append("<tr><th colspan='2'>").append(featureName).append("</th></tr>");
 
             for (JsonElement scenarioElement : scenarios) {
+                totalScenarios++; // Increment total scenarios count
                 JsonObject scenario = scenarioElement.getAsJsonObject();
                 String scenarioName = scenario.get("name").getAsString();
                 JsonArray steps = scenario.getAsJsonArray("steps");
@@ -102,28 +99,28 @@ public void generateHTMLReport() {
                 htmlBuilder.append("<td>").append(scenarioName).append("</td>");
 
                 for (JsonElement stepElement : steps) {
+                    totalSteps++; // Increment total steps count
                     JsonObject step = stepElement.getAsJsonObject();
                     JsonObject result = step.getAsJsonObject("result");
                     String status = result.get("status").getAsString();
                     if (status.equalsIgnoreCase("failed")) {
                         hasFailedStep = true;
-                        totalFailedSteps++;
+                        totalFailedSteps++; // Increment total failed steps count
                     } else {
-                        totalPassedSteps++;
+                        totalPassedSteps++; // Increment total passed steps count
                     }
                 }
 
                 if (hasFailedStep) {
                     totalFailedScenarios++;
                     hasFailedScenario = true;
-                    // Set background color of scenario cell and status cell based on whether any step has failed
                     htmlBuilder.append("<td style='background-color: #F9502B;'>FAILED</td>");
                 } else {
                     totalPassedScenarios++;
                     htmlBuilder.append("<td style='background-color: #2BF96D;'>PASSED</td>");
                 }
 
-                htmlBuilder.append("</tr>\n");
+                htmlBuilder.append("</tr>");
             }
 
             if (hasFailedScenario) {
@@ -132,20 +129,22 @@ public void generateHTMLReport() {
                 totalPassedFeatures++;
             }
 
-            htmlBuilder.append("</tbody></table>\n");
-            htmlBuilder.append("</br>");
-            // Increment total scenarios count
-            totalScenarios += scenarios.size();
+            htmlBuilder.append("</table>");
         }
 
         // Generate test summary table
-        htmlBuilder.append("<h2>Test Summary</h2>\n");
-        htmlBuilder.append("<table border='1' style='border-collapse: collapse;'>\n");
-        htmlBuilder.append("<thead><tr><th>Type</th><th>Total Count</th><th>Total Passed</th><th>Total Failed</th></tr></thead>\n");
-        htmlBuilder.append("<tbody><tr><td>Features</td><td>").append(totalFeatures).append("</td><td>").append(totalPassedFeatures).append("</td><td>").append(totalFailedFeatures).append("</td></tr></tbody>\n");
-        htmlBuilder.append("<tbody><tr><td>Scenarios</td><td>").append(totalScenarios).append("</td><td>").append(totalPassedScenarios).append("</td><td>").append(totalFailedScenarios).append("</td></tr></tbody>\n");
-        htmlBuilder.append("<tbody><tr><td>Steps</td><td>").append(totalSteps).append("</td><td>").append(totalPassedSteps).append("</td><td>").append(totalFailedSteps).append("</td></tr></tbody>\n");
-        htmlBuilder.append("</table>\n");
+        htmlBuilder.append("<h3 style='text-align:center;margin-bottom:20px;'>Test Summary</h3>");
+        htmlBuilder.append("<table>");
+        htmlBuilder.append("<tr><th>Type</th><th>Total Count</th><th>Total Passed</th><th>Total Failed</th></tr>");
+        htmlBuilder.append("<tr><td>Features</td><td>").append(totalFeatures).append("</td><td>").append(totalPassedFeatures).append("</td><td>").append(totalFailedFeatures).append("</td></tr>");
+        htmlBuilder.append("<tr><td>Scenarios</td><td>").append(totalScenarios).append("</td><td>").append(totalPassedScenarios).append("</td><td>").append(totalFailedScenarios).append("</td></tr>");
+        htmlBuilder.append("<tr><td>Steps</td><td>").append(totalSteps).append("</td><td>").append(totalPassedSteps).append("</td><td>").append(totalFailedSteps).append("</td></tr>");
+        htmlBuilder.append("</table>");
+
+        // Footer
+        htmlBuilder.append("<footer style='border-collapse: collapse;width: 80%;max-width: 600px;margin: 0 auto;'><p>Thanks & Regards,</br>Automation Team.</p></footer>");
+
+        htmlBuilder.append("</body></html>");
 
         // Save HTML report to file
         BufferedWriter writer = new BufferedWriter(new FileWriter(HTML_FILE_PATH));
